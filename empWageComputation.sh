35e84eae64127b4c0c2d244eba6e37eb9a90eb44
Kalyani <kalyanirane19@gmail.com>
2020-01-21 17:46:25 +0530

    ADDED : Calculating Wage On Hour Basis

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,22 +6,26 @@ WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
 PART_TIME_HOUR=4
 
-
-day=1
-while [ $day -le 20 ]
+hours=0
+totalHours=0
+day=0
+while [[ $day -lt 20 && $totalHours -lt 100 ]]
 do
 	echo "Day = $day"
 	result=$((RANDOM % 3))
 	case $result in
-				1)echo "Employee is work full time."
-					echo "Employee Daily Wage is = $((FULL_DAY_HOUR * WAGE_PER_HOUR))"
+				1)salary=$((salary + ((FULL_DAY_HOUR * WAGE_PER_HOUR))))
+					hours=8
+					((day++))
 					;;
-				2)echo "Employee is work part time."
-				  echo "Employee Daily Wage is = $((PART_TIME_HOUR * WAGE_PER_HOUR))"
+				2)salary=$((salary + ((PART_TIME_HOUR * WAGE_PER_HOUR))))
+					hours=4
+					((day++))
 					;;
-				0)echo "Employee is absent."
-					echo "Employee Daily Wage is = 0"
+				0)salary=$((salary + 0))
+					hours=0
 					;;
 	esac
-	((day++))
+	totalHours=$((totalHours + hours))
 done
+echo "Monthly Wage is: $salary"
